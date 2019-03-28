3096a31e8f7af538eb46448dc92e1a2d24b9b050
Shahzad Tariq <shahzad@nxvt.com>
2019-03-28 17:20:56 +0500

    added statistics methods and updated README doc

--- lib/devise_activity/stats.rb
+++ lib/devise_activity/stats.rb
@@ -3,39 +3,43 @@ module DeviseActivity
     class << self
 
       def most_visits_by
-        DeviseActivity::Navigation.joins(:session).group("sessions.user_id").count.max_by{|k, v| v }.first
+        DeviseActivity.configuration.devise_model_name.classify.constantize.find(DeviseActivity::Navigation.joins(:session).group("sessions.user_id").count.max_by{|k, v| v }.first) rescue nil
       end
 
       def least_visits_by
-        DeviseActivity::Navigation.joins(:session).group("sessions.user_id").count.min_by{|k, v| v }.first
+        DeviseActivity.configuration.devise_model_name.classify.constantize.find(DeviseActivity::Navigation.joins(:session).group("sessions.user_id").count.min_by{|k, v| v }.first) rescue nil
       end
 
       def most_popular_action
-
+        DeviseActivity::Navigation.group("action_name").count.max_by{|k, v| v}.first
       end
 
       def least_popular_action
-
+        DeviseActivity::Navigation.group("action_name").count.min_by{|k, v| v}.first
       end
 
       def most_popular_controller
-
+        DeviseActivity::Navigation.group("controller_name").count.max_by{|k, v| v}.first
       end
 
       def least_popular_controller
-
+        DeviseActivity::Navigation.group("controller_name").count.min_by{|k, v| v}.first
       end
 
       def most_visited_page
+        DeviseActivity::Navigation.group("page_url").count.max_by{|k, v| v}.first
+      end
 
+      def least_visited_page
+        DeviseActivity::Navigation.group("page_url").count.min_by{|k, v| v}.first
       end
 
       def most_sign_ins_by
-
+        DeviseActivity.configuration.devise_model_name.classify.constantize.find((DeviseActivity::Session.group("#{DeviseActivity.configuration.devise_model_name}_id").count.max_by{|k, v| v }.first) rescue nil
       end
 
-      def sign_ins_count user
-
+      def total_sign_ins obj
+        Session.where("#{DeviseActivity.configuration.devise_model_name}_id = ?", obj.id).count
       end
 
     end
